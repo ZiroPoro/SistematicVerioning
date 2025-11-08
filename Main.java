3c703355efdac071dab57f667fe3425a1ee9f385
ZiroPoro <andrew777chzen@gmail.com>
2025-11-08 12:50:44 +0300

    Count from 0 to 10 in Main.py was added and the new print was added in Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
+    
     public static void main(String[] args) {
+        String stroke = "Приветственное сообщение";
         System.out.println("Hello my world");
         for(int i=0;i<10;i++){
             System.out.println(i);
         }
+        System.out.println(stroke);
     }
+
 }
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
+i=0;
 print("hello")
 print("The changes are so so so sos sos so so beautiful")
+for i in 10:
+    print(i)
